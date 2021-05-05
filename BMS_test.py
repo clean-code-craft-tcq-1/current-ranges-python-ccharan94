31bf6ac4f68b6458c088a2aa2a7e64086f7700f4
ccharan94 <77478060+ccharan94@users.noreply.github.com>
2021-05-06 02:09:28 +0530

    Update BMS_test.py

--- BMS_test.py
+++ BMS_test.py
@@ -3,7 +3,7 @@ import BMSMonitoring
 
 class test_bms_ranges(unittest.TestCase):
   def test_sequence_count(self): #passing test
-    self.assertTrue(BMSMonitoring.getSequenceCount([3, 3, 5, 4, 10, 11, 12]) == { "3-5" : "4", "10-12" : "3" } )
+    self.assertTrue(BMSMonitoring.getRangeCount([3, 3, 5, 4, 10, 11, 12]) == { "3-5" : "4", "10-12" : "3" } )
   
   def test_valid_input(self):    #failing test
     self.assertTrue(BMSMonitoring.isValidInput("3,3,5,4,10,11,12") == True ) #Passes even though string is passed instead of list
