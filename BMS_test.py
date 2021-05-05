fdfa85b2d28dbb511366ea3dac18f11428d785ed
ccharan94 <77478060+ccharan94@users.noreply.github.com>
2021-05-06 00:13:28 +0530

    Update BMS_test.py

--- BMS_test.py
+++ BMS_test.py
@@ -1,1 +1,12 @@
+import unittest
+import BMSMonitoring
 
+class test_bms_ranges(unittest.TestCase):
+  def test_sequence_count(self):
+    self.assertTrue(BMSMonitoring.getSequenceCount([3, 3, 5, 4, 10, 11, 12]) == { "3-5" : "4", "10-12" : "3" }
+  
+  def test_valid_input(self)
+    self.assertTrue(BMSMonitoring.isValidInput([]) == True )
+
+if __name__ =='__main__':
+  unittest.main()
