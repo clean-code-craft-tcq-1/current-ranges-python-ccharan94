44687f6fefc9c656fad949140abd25f7307e397a
ccharan94 <77478060+ccharan94@users.noreply.github.com>
2021-05-06 00:27:29 +0530

    Update BMS_test.py

--- BMS_test.py
+++ BMS_test.py
@@ -2,11 +2,11 @@ import unittest
 import BMSMonitoring
 
 class test_bms_ranges(unittest.TestCase):
-  def test_sequence_count(self):
-    self.assertTrue(BMSMonitoring.getSequenceCount([3, 3, 5, 4, 10, 11, 12]) == { "3-5" : "4", "10-12" : "3" }
+  def test_sequence_count(self): #passing test
+    self.assertTrue(BMSMonitoring.getSequenceCount([3, 3, 5, 4, 10, 11, 12]) == { "3-5" : "4", "10-12" : "3" } )
   
-  def test_valid_input(self)
-    self.assertTrue(BMSMonitoring.isValidInput("3,3,5,4,10,11,12") == True )
+  def test_valid_input(self):    #failing test
+    self.assertTrue(BMSMonitoring.isValidInput("3,3,5,4,10,11,12") == True ) #Passes even though string is passed instead of list
 
 if __name__ =='__main__':
   unittest.main()
